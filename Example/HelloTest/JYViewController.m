0f275973bafba32f11a0b163367c757f82e7df16
HadesSura <1905058100@qq.com>
2020-07-03 16:58:00 +0800

    组件初始化

--- Example/HelloTest/JYViewController.m
+++ Example/HelloTest/JYViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "JYViewController.h"
-
+#import <HelloMethod.h>
 @interface JYViewController ()
 
 @end
@@ -18,6 +18,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+   NSString *str = [HelloMethod ouputHello];
+    NSLog(@"test:::%@",str);
+    UILabel *a = [UILabel new];
+    a.text = str;
+    a.font = [UIFont systemFontOfSize:18];
+    a.textColor = [UIColor blueColor];
+    a.frame = CGRectMake(30, 120, 200, 30);
+    [self.view addSubview:a];
+    
+    UIImageView *imagev = [[UIImageView alloc] initWithImage:[HelloMethod ouputImage]];
+    imagev.frame = CGRectMake(100, 200, 90, 90);
+    [self.view addSubview:imagev];
 }
 
 - (void)didReceiveMemoryWarning
---
+++ Example/Pods/Target Support Files/HelloTest/HelloTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HelloTest : NSObject
+@end
+@implementation PodsDummy_HelloTest
+@end
